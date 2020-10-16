2a6250c5224124028fb3c8a9968c2ff2cc66fade
rk <raymondkalonji@gmail.com>
2020-10-16 12:10:56 -0400

    feat: adding oauth app info

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-2whsuv3c.us.auth0.com',            // Auth0 domain
+  clientId: 'IEPNbzZpRZRFyEyyMWF3vgWP49f3n0HN',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
