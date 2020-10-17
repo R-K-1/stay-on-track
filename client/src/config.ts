6db45fbd4431c9e6508346c0536c425777a14752
rk <raymondkalonji@gmail.com>
2020-10-16 20:54:48 -0400

    feat: updating app creds

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'pjragy8zdf'
+const apiId = 'vvvgtbtmpe'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-2whsuv3c.us.auth0.com',            // Auth0 domain
-  clientId: 'IEPNbzZpRZRFyEyyMWF3vgWP49f3n0HN',          // Auth0 client id
+  clientId: 'rKUcBuJaQJrR8f16XtyoGoer5CCWFdNg',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
